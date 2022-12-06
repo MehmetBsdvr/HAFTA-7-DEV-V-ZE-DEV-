4c674700f20e2cc363667eb51dcedb418afecfb9
MehmetBsdvr <117525537+MehmetBsdvr@users.noreply.github.com>
2022-12-06 22:10:05 +0300

    Update SORUB23.java

--- ÖDEV7/src/SORUB23.java
+++ ÖDEV7/src/SORUB23.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/*
-Dost sayı yazdıran program
-*/
+
+
+
 
 /**
  *
@@ -18,6 +18,8 @@ public class SORUB23 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /*Dost sayı yazdıran program İki sayı birbirinin kendisi hariç bölenleri toplamına eşitse bu sayılara arkadaş sayılar denir.
+         ... 220 : 1 + 2 + 4 + 5 + 10 + 11 + 20 + 22 + 44 + 55 + 110 = 284*/
         Scanner input =new Scanner(System.in);
         
         System.out.println("Sayı1 giriniz ");
